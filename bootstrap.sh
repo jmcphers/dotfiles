0183dd7964189907b487b54bcad7e26e536132e7
Jonathan McPherson <jonathan@rstudio.com>
2015-10-20 22:34:38 -0700

    use Windows-specific paths for vim config files/dirs

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,27 @@
 git config --global user.email "jonathan@rstudio.com"
 git config --global user.name "Jonathan McPherson"
 
+VIMRT=~/.vim
+VIMRC=~/.vimrc
+if [[ "$OSTYPE" == "msys" ]]; then
+    VIMRT=~/vimfiles
+    VIMRC=~/_vimrc
+fi
+
 # install vundle itself (needed so vundle can install other plugins)
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git $VIMRT/bundle/Vundle.vim
 
 # install vim color schemes (vundle has a hard time with this)
-git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/vim-colorschemes
-mv ~/.vim/vim-colorschemes/colors ~/.vim/colors
-rm -rf ~/.vim/vim-colorschemes
+git clone https://github.com/flazz/vim-colorschemes.git $VIMRT/vim-colorschemes
+mv $VIMRT/vim-colorschemes/colors $VIMRT/colors
+rm -rf $VIMRT/vim-colorschemes
 
 # use vim color schemes for neovim
 mkdir -p ~/.nvim
-ln -s ~/.vim/colors ~/.nvim/colors
+ln -s $VIMRT/colors ~/.nvim/colors
 
 # link dotfiles
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vimrc $VIMRC
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 # create vim swapfile targets
