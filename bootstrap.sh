79a9010195f0074d1ed0351f735c4cf3b53c14a5
Jonathan McPherson <jonathan@rstudio.com>
2016-11-30 10:27:03 -0800

    update bootstrap script for neovim + vim-plug

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,20 +9,11 @@ if [[ "$OSTYPE" == "msys" ]]; then
     VIMRC=~/_vimrc
 fi
 
-# install vundle itself (needed so vundle can install other plugins)
-git clone https://github.com/gmarik/Vundle.vim.git $VIMRT/bundle/Vundle.vim
-
-# install vim color schemes (vundle has a hard time with this)
-git clone https://github.com/flazz/vim-colorschemes.git $VIMRT/vim-colorschemes
-mv $VIMRT/vim-colorschemes/colors $VIMRT/colors
-rm -rf $VIMRT/vim-colorschemes
+# install vim-plug
+curl -fLo $VIMRT/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# use vim color schemes for neovim
-mkdir -p ~/.nvim
-ln -s $VIMRT/colors ~/.nvim/colors
 
 # link dotfiles
 ln -s ~/dotfiles/.vimrc $VIMRC
@@ -31,6 +22,11 @@ ln -s ~/dotfiles/.todo.cfg ~/.todo.cfg
 ln -s ~/dotfiles/UltiSnips ~/.vim/UltiSnips
 ln -s ~/dotfiles/.muttrc ~/.muttrc
 
-# create vim swapfile targets
+# use same vimrc for neovim and regular vim (TODO: this is unix-only)
+mkdir -p ~/.config/nvim
+ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+
+# create vim swapfile targets -- this keeps directories tidy
 mkdir -p ~/.vimbackup
 mkdir -p ~/.vimswap
+
