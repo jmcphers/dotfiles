dbb5a1216ab75b1982dce294e82a244c2f7c94a8
Jonathan McPherson <jonathan@rstudio.com>
2018-03-26 13:52:57 -0700

    update bootstrap script for the harsh realities of windows

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# figure out where we live
+DOTFILES="$(dirname ${BASH_SOURCE[0]})"
+
 # set up git identity
 git config --global user.email "jonathan@rstudio.com"
 git config --global user.name "Jonathan McPherson"
@@ -11,20 +14,27 @@ if [[ "$OSTYPE" == "msys" ]]; then
     VIMRC=~/_vimrc
 
     # install vim-plug for neovim
-    mkdir -p ~/AppData/Local/nvim/autoload
-    curl -o ~/AppData/Local/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    if [ ! -d ~/AppData/Local/nvim/autoload ]; then
+       mkdir -p ~/AppData/Local/nvim/autoload
+       curl -o ~/AppData/Local/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    fi
 fi
 
 # install vim-plug
-curl -fLo $VIMRT/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -d $VIMRT/autoload/plug.vim ]; then 
+    curl -fLo $VIMRT/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # install tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 if [[ "$OSTYPE" == "msys" ]]; then
-    # use same vimrc for neovim and regular vim
-    ln -s ~/dotfiles/vim/.vimrc $VIMRC
-    ln -s ~/dotfiles/init-windows.vim ~/AppData/Local/nvim/init.vim
+    NEOVIMRC=~/AppData/Local/nvim
+    mkdir -p "$NEOVIMRC"
+    cp "$DOTFILES/vim/.vimrc" "$VIMRC" 
+    cp "$DOTFILES/init-windows.vim" "$NEOVIMRC/init.vim" 
 else
     # use stow to set up symlinks
     stow neovim
@@ -40,13 +50,16 @@ mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/swap
 mkdir -p ~/.vim/undo
 
-# make "git gtags" work
-git config --global alias.gtags '!.git/hooks/gtags'
+# setup vim with gtags (not available on windows)
+if [[ "$OSTYPE" != "msys" ]]; then
+    # make "git gtags" work
+    git config --global alias.gtags '!.git/hooks/gtags'
 
-# set up git template folder and init with gtags
-GIT_TEMPLATE="~/.config/git/template"
-mkdir -p $GIT_TEMPLATE/hooks
-git config --global init.templatedir "$GIT_TEMPLATE"
-~/dotfiles/bin/setup-git-gtags $GIT_TEMPLATE
+    # set up git template folder and init with gtags
+    GIT_TEMPLATE="~/.config/git/template"
+    mkdir -p $GIT_TEMPLATE/hooks
+    git config --global init.templatedir "$GIT_TEMPLATE"
+    $DOTFILES bin/setup-git-gtags $GIT_TEMPLATE
+fi
 
 
