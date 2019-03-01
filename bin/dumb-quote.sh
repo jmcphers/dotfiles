2cf625a5e4a11d3e6b9dc48d71d4d898a85d45ec
Jonathan McPherson <jonathan@rstudio.com>
2019-03-01 12:55:00 -0800

    update dumb quote script

--- bin/dumb-quote.sh
+++ bin/dumb-quote.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+# This script filters stdin and replaces a large number of "smart" typographic
+# characters with dumb, plain ASCII equivalents. Some of these characters are
+# non-printable, so edit carefully.
+#
+# The goal of the script is to make it easy to turn content pasted from
+# programs like Word into programs that can't handle the extended characters.
+
 cat - |
     sed -e "s/‘/'/g" | \
     sed -e "s/’/'/g" | \
     sed -e "s/…/.../g" | \
     sed -e "s/“/\"/g" | \
-    sed -e "s/”/\"/g" 
+    sed -e "s/”/\"/g" | \
+    sed -e "s/ / /g"
 
